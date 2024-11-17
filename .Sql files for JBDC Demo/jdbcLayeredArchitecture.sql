f78b52d2d08bfc1315e75e82db81a9ebf6909530
SuraCdac <sura.cdac@gmail.com>
2024-11-17 11:53:55 +0530

    servlet with jdbc connection

--- .Sql files for JBDC Demo/jdbcLayeredArchitecture.sql
+++ .Sql files for JBDC Demo/jdbcLayeredArchitecture.sql
@@ -14,10 +14,10 @@ drop table if exists product;
 create table product(id int, name varchar(20), qty int, price int, expdate date, cid int);
 
 insert into product values(1, 'product1', 10, 100, '2025-10-20', 1),
-(1, 'product2', 20, 200, '2027-10-30', 1),
-(1, 'product3', 30, 300, '2023-09-17', 1),
-(1, 'product4', 40, 400, '2024-08-18', 1),
-(1, 'product5', 50, 500, '2026-07-12', 1),
-(1, 'product6', 60, 600, '2025-08-24', 1),
-(1, 'product7', 70, 700, '2024-05-20', 1),
-(1, 'product8', 80, 800, '2022-12-02', 1);
+(2, 'product2', 20, 200, '2027-10-30', 2),
+(3, 'product3', 30, 300, '2023-09-17', 3),
+(4, 'product4', 40, 400, '2024-08-18', 4),
+(5, 'product5', 50, 500, '2026-07-12', 5),
+(6, 'product6', 60, 600, '2025-08-24', 6),
+(7, 'product7', 70, 700, '2024-05-20', 7),
+(8, 'product8', 80, 800, '2022-12-02', 8);
