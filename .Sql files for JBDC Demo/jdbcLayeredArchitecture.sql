44ff0204122620d2855ba03565acd799a2885346
SuraCdac <sura.cdac@gmail.com>
2024-11-26 12:53:58 -0800

    user data management using jsp and servlet using MVC

--- .Sql files for JBDC Demo/jdbcLayeredArchitecture.sql
+++ .Sql files for JBDC Demo/jdbcLayeredArchitecture.sql
@@ -11,16 +11,25 @@ insert into userinfo values('admin1', 'admin1', 'admin'),('user1', 'user1', 'use
 
 drop table if exists product;
 
-create table product(id int auto_increment PRIMARY KEY, name varchar(20), qty int, price DECIMAL, expdate date, cid int);
+create table product(id int PRIMARY KEY, name varchar(20), qty int, price DECIMAL, expdate date, cid int);
 
-insert into product(name, qty, price, expdate, cid) values('product1', 10, 100, '2025-10-20', 1),
-('product2', 20, 200, '2027-10-30', 2),
-('product3', 30, 300, '2023-09-17', 3),
-('product4', 40, 400, '2024-08-18', 4),
-('product5', 50, 500, '2026-07-12', 5),
-('product6', 60, 600, '2025-08-24', 6),
-('product7', 70, 700, '2024-05-20', 7),
-('product8', 80, 800, '2022-12-02', 8);
+insert into product values(1, 'Lays', 10, 20, '2025-10-20', 1),
+(2, 'parle', 20, 5, '2027-10-30', 2),
+(3, 'thumbs up', 30, 25, '2023-09-17', 3),
+(4, 'dairy milk', 40, 100, '2024-08-18', 4),
+(5, 'Pringles', 50, 120, '2026-07-12', 1),
+(6, 'Good day', 60, 20, '2025-08-24', 2),
+(7, 'coco cola', 70, 25, '2024-05-20', 3),
+(8, 'amul', 80, 100, '2022-12-02', 4);
+
+drop table if exists catagory;
+
+create table catagory(id int, cname varchar(30), cdesc varchar(50));
+
+insert into catagory values(1, 'chips', 'crunchy munchy'),
+(2, 'biscuit', 'Salty and sweet'),
+(3, 'cold drinks', 'Thanda thanda cool cool'),
+(4, 'chocalte', 'sweety');
 
 drop table if exists person;
 
